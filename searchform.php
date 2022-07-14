bcf41e2476a878c6aa9689e170666b3aa2ff364f
sajjee68 <kashani.sajjad@gmail.com>
2022-07-15 00:55:20 +0430

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
       class="border-2 border-border w-23 lg-w-50 rounded-xs flex flex-row">
     <input type="hidden" name="post_type" value="product"/>
     <input id="mdr_search_nonce" type="hidden" name="_wpnonce" value="<?php echo wp_create_nonce( 'wp_rest' ) ?>"/>
-    <input id="mdr_search_product"
+    <input id="mdr_search_product" autocomplete="off"
            class="pt-0/5 pb-0/7 lg-pt-1/1 lg-pb-1/2 flex-1 text-sm lg-text-base leading-2/2 text-gray-600 pr-1/3 lg-pr-2/1 rounded-xs"
            type="search" name="s" placeholder="جستجو در محصولات ..."/>
     <button type="submit"
